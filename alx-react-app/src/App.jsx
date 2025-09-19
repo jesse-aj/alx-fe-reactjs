eb4ecbd48ad964ab7243b2e7fd916443ebfd2f51
jesse-aj <appiahjesse123@gmail.com>
2025-09-19 22:38:39 +0000

    added  a react component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -13,21 +13,21 @@ function App() {
 
   return (
     <>
-      {/* <div>
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div> */}
+      </div> 
 
       <WelcomeMessage /> 
       <Header/>
       <MainContent/>
       <Footer/> 
 
-      {/* <h1>Vite + React</h1>
+    <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -38,7 +38,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p> 
     </>
   );
 }

dd15513c96aebf18a01a9ac54b99bed6d8db8311
jesse-aj <appiahjesse123@gmail.com>
2025-09-19 19:46:00 +0000

    created a react component

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello Everyone I am learning React</h1>
+      <p>This is a simple JSX component</p> 
+    </div>
+  );
+}
+
+export default WelcomeMessage;

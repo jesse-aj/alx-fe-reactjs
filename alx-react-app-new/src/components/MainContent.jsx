2aa257a2ac26f1cd1ec99c5f68df755374a1b991
jesse-aj <appiahjesse123@gmail.com>
2025-09-24 15:04:39 +0000

    made changes

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent () {
 return (
 <main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
+  <p style= {{color: "aqua"}}>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 );
 
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,15 @@
 function UserProfile  (props)
 {
     return (
-         <div className="user-profile">
-            <h2>{props.name}</h2>
-            <p>Age = {props.age}</p>
-            <p>Bio = {props.bio}</p>
+         <div className="user-profile" style= {{
+           backgroundColor: "white",
+           padding: "20px",
+           border: "3px solid aqua",
+           margin: "20px"
+         }}>
+            <h2 style = {{color:"blue"}}>{props.name}</h2>
+            <p> <span style={{fontWeight: "bold", color:"blue" , fontSize:"20px"}}>Age : {props.age}</span></p>
+            <p> <span style={{fontSize: "19px", fontWeight: "bold", color:"blue"}}>Bio :{props.bio}</span></p>
          </div>
     );
 } 

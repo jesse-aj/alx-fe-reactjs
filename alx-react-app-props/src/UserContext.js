d18de9c986b25fba744cc2b7c826922c6f55c00b
jesse-aj <appiahjesse123@gmail.com>
2025-09-25 16:03:41 +0000

    Refractored the props

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,7 @@
+import React from "react";
 import { createContext } from "react";
 
-const UserContext = createContext();
+const UserContext = React.createContext();
 
 export default UserContext;
 

9d998f94fd7218867022274f370bdcacd4e66e61
jesse-aj <appiahjesse123@gmail.com>
2025-09-25 17:02:28 +0000

    Refractored the props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
-function UserProfile  (props)
-{
-    return (
-         <div className="user-profile">
-            <h2>{props.name}</h2>
-            <p>Age = {props.age}</p>
-            <p>Bio = {props.bio}</p>
-         </div>
-    );
-} 
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
 
+function UserProfile (){
+
+    const userData = useContext(UserContext)
+    return (
+     <div>
+           <h2>User Profile</h2>
+           <p>Name: {userData.name}</p>
+           <p>Email: {userData.email}</p>
+     </div>
+    );
+}
 
 export default UserProfile;

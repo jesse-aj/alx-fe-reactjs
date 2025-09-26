cde4fc6b44301fe2650ded947a8e2f2381aef124
jesse-aj <appiahjesse123@gmail.com>
2025-09-26 06:40:44 +0000

    fixed components errors

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#ddd" }}>
+    <nav style={{ backgroundColor : "aqua", display : "flex", justifyContent : "center"}}>
       <Link to="/">Home</Link> |{" "}
       <Link to="/about">About</Link> |{" "}
       <Link to="/services">Services</Link> |{" "}

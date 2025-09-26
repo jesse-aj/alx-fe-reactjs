d3564c09c831134c449e3bce61334dcc52ad8e3c
jesse-aj <appiahjesse123@gmail.com>
2025-09-26 06:21:55 +0000

    fixed components errors

---
+++ my-company/src/assets/Components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#333",
+        color: "white",
+        padding: "10px",
+        textAlign: "center",
+        marginTop: "20px"
+      }}
+    >
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;

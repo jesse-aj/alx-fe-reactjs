403dfa715bc6124d5227b028f8356e437993cb9a
jesse-aj <appiahjesse123@gmail.com>
2025-10-03 12:56:21 +0000

    fixed Routing error

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
+    <Router>
       <h1> Recipe Add</h1>
       <Routes>
         <Route path = "/" element = {
@@ -20,7 +20,7 @@ function App() {
 <Route path = "/recipes/:id" element = {<RecipeDetails />} />
 </Routes>
     
-    </div>
+    </Router>
   );
 
 }

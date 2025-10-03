6a580fbc70262d701fa630bef7799e168de1511b
jesse-aj <appiahjesse123@gmail.com>
2025-10-03 15:17:47 +0000

    Update the Zustand Store for Search and Filtering

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,20 @@ import {create} from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
+
+  searchTerm:  "",
+  setSearchTerm: (term) => set ({searchTerm: term}),
+
+
+  filteredRecipes: [],
+filteredRecipes : () => set ( state => ({
+  filteredRecipes:state.recipes.filter(recipe => 
+    recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+  )
+})),
+
+
+
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
 
 

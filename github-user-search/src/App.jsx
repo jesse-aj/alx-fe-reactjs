3324f666e8caaa543c802b08967bd991cac91115
jesse-aj <appiahjesse123@gmail.com>
2025-10-06 13:04:26 +0000

    set up environment and to use reactjs for API calls

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+  
+
+function App() {
+
+
+return (
+  <Router>
+    <div>
+      <Link to="">     </Link>
+      <Link to="">     </Link>
+      <Link to="">     </Link>
+      <Link to="">     </Link>
+
+
+
+
+      <Routes>
+        <Route path= "" element = {}/>
+        <Route path= "" element = {}/>
+        <Route path= "" element = {}/>
+        <Route path= "" element = {}/>
+
+
+      </Routes>
+    </div>
+  </Router>
+);
+}
+
+export default App;

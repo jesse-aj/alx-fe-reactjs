502ff590d2abeac0a021ef3ff0cab7a09214a3e5
jesse-aj <appiahjesse123@gmail.com>
2025-10-11 02:42:04 +0000

    added anition and transition effects on my components

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 sm:h-24 sm:w-24 mx-auto" />
-      <h1 className=" text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
+    <div className="bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <img src="https://via.placeholder.com/150" alt="User" className="bg-red-700 rounded-full md:w-36 md:h-36 sm:h-24 sm:w-24 mx-auto hover:scale-100 transition-transform duration-300 ease-in-out" />
+      <h1 className=" text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">John Doe</h1>
       <p className="text-gray-600 md:text-base sm:text-sm ">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );

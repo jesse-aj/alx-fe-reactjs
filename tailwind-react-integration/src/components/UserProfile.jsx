e1531647eb7791cec95949ff840809a6d49ab77e
jesse-aj <appiahjesse123@gmail.com>
2025-10-11 02:25:53 +0000

    added responsivenes to the elements for small and large screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:h-24 w-36 h-36 sm:h-24 w-24 mx-auto" />
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 h-36 sm:h-24 w-24 mx-auto" />
       <h1 className=" text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
       <p className="text-gray-600 md:text-base sm:text-sm ">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>

3be7516d282520bec13140b4a57420f2c81d2d06
jesse-aj <appiahjesse123@gmail.com>
2025-10-13 13:27:48 +0000

    now my react app can fetch daat and display te details of the recipe whrn the view button recipe is clicked

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -20,13 +20,45 @@ function RecipeDetail() {
   return (
     <div className="p-8 bg-gray-50 min-h-screen flex justify-center">
       <div className="bg-white rounded-2xl shadow-lg max-w-2xl w-full p-6">
+
+        {/* Recipe Image */}
         <img
           src={recipe.image}
           alt={recipe.title}
           className="w-full h-64 object-cover rounded-lg mb-6"
         />
+
+        {/* Title and Summarry */}
         <h1 className="text-3xl font-bold text-gray-800 mb-4">{recipe.title}</h1>
         <p className="text-gray-600 mb-6">{recipe.summary}</p>
+         
+
+         {/* Instructions */}
+         <div className="mb-6">
+            <h2 className="text-xl font-semibold text-gray-700 mb-2">Ingredients</h2>
+            <ul className="list-disc list-inside text-gray-600 space-y-1">
+                {recipe.ingredients.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+
+            </ul>
+
+         </div>
+        
+
+        {/* Instructions */}
+
+        <div className="mb-6">
+            <h2 classname="text-xl font-semibold text-gray-700 mb-2">Ingredients</h2>
+            <ol className="list-decimal list-inside text-gray-600 space-y-1">
+                {recipe.instructions.map ((step, index) => (
+                <li key = {index}>{step}</li>
+                ))}
+            </ol>
+
+
+        </div>
+    
       </div>
     </div>
   );

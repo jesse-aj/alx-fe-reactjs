d9e4e48a1e8c53fedcc56a187e6b8eec71b941d3
jesse-aj <appiahjesse123@gmail.com>
2025-10-13 04:39:46 +0000

    configured tailwind css in my application

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "./App.css"
+
+function App() {
+
+
+  return (
+
+    <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
+  Click Me
+</button>
+
+)
+};
+
+export default App
